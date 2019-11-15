e6d3c06b1312c39c8d08c8ae789e9cd8ab8c51a1
ceciliakemiac <ceciliakemiac2@gmail.com>
2019-11-15 12:36:00 -0300

    homens e mulheres

---
+++ teste.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+
+recife <- read_csv("basico-censo-recife-utf8.csv") %>%
+  select(Cod_setor)
+
+domicilios <- read_csv2("CSV/Domicilio01_PE.csv") %>%
+  filter(Cod_setor %in% recife$Cod_setor) %>%
+  subset(select = c(Cod_setor, V062:V099)) %>%
+  mutate_if(is.character,as.numeric)
+
+homens <- domicilios %>%
+  subset(select = c(Cod_setor, V062:V080)) %>%
+  mutate(sum_homens = rowSums(.[2:20])) %>%
+  select(Cod_setor, sum_homens)
+
+mulheres <- domicilios %>%
+  subset(select = c(Cod_setor, V081:V099)) %>%
+  mutate(sum_mulheres = rowSums(.[2:20])) %>%
+  select(Cod_setor, sum_mulheres)
+
+domis <- homens %>%
+  inner_join(mulheres, by = "Cod_setor") %>% 
+  write_csv("domicilios.csv")
